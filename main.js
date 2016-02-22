f66c27a0f3e2c82e5711dcb510c5c3e61f10bc7f
Evan Palmer <evjpalmer@gmail.com>
2016-02-21 21:33:44 -0800

    add jQuery code including JSONP ajax call

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
-var DATA = new FormData();
-data.append("method", "getQuote");
-data.append("format", "json");
-data.append("lang", "en");
+$(document).ready(function () {
+    function getQuote() {
+      var url = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?';
+      $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'jsonp',
+        crossDomain: true,
+        jsonp: displayQuote,
+        success: function (results) {
+          console.log("success: ", results);
+          displayQuote(results);
+        },
+        error: function (results) {
+          console.log("error: ", results);
+        }
+      });
+    }
 
-var request = new XMLHttpRequest();
-request.onload = function () {
-    "use strict";
-    console.log(JSON.parse(this.responseText));
-}
-request.open("POST", "http://api.forismatic.com/api/1.0/");
-request.send(data);
+  function displayQuote(quote) {
+    console.log("display quote running");
+    var $qBody = $("#body"),
+        $qAuthor = $("#author");
+    $qBody.text(quote.quoteText);
+    if (quote.quoteAuthor !== "") {
+      $qAuthor.text(quote.quoteAuthor);
+    }
+    else {
+      $qAuthor.text("unknown");
+    }
+  }
+
+  var $getQuoteBtn = $("#get-quote-btn");
+  $getQuoteBtn.on("click", getQuote);
+});
